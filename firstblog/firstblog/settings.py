dbe6e3a1f646508b28cabb5a11673fda1f30a2b2
Amey rane <ameyrane98@ameyrane.1998@gmail.com>
2021-09-30 16:31:39 +0530

    commit

--- firstblog/firstblog/settings.py
+++ firstblog/firstblog/settings.py
@@ -86,13 +86,14 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+    },  
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
